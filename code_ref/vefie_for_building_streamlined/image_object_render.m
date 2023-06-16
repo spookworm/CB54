10ae205ad1eb47c5beb295cb9a8c780d3e463537
AJMcE <anthonyjames.mcelwee@gmail.com>
2023-06-16 19:22:59 +0100

    MATLAB dictionary implementation

--- code_ref/vefie_for_building_streamlined/image_object_render.m
+++ code_ref/vefie_for_building_streamlined/image_object_render.m
@@ -1,13 +1,13 @@
-function image_object_render(image_object, map, materials, markerColor, title_str)
-image_object_render = ind2rgb(image_object, map);
+function image_object_render(image_object, materials_master, markerColor, title_str)
+image_object_render = ind2rgb(image_object, materials_master.('map'));
 figure
 imagesc(image_object_render, 'XData', 1/2, 'YData', 1/2)
 title(title_str)
 legend
 set(gcf, 'units', 'normalized', 'outerposition', [0, 0, 1, 1])
 hold on
-L = plot(ones(height(materials)), 'LineStyle', 'none', 'marker', 's', 'visible', 'on');
+L = plot(ones(height(materials_master.('name'))), 'LineStyle', 'none', 'marker', 's', 'visible', 'on');
 set(L, {'MarkerFaceColor'}, markerColor, {'MarkerEdgeColor'}, markerColor);
-colormap(map)
-legend(materials)
+colormap(materials_master.('map'))
+legend(materials_master.('name'))
 end
