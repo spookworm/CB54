82d91a8314bb4bcec21750694c6b348eb68712b9
AJMcE <anthonyjames.mcelwee@gmail.com>
2023-06-16 21:59:15 +0100

    Update scene_gen.py

--- lib/scene_gen.py
+++ lib/scene_gen.py
@@ -313,3 +313,87 @@ def basis_wave_number(resolution_information, image_geometry_materials_full, ima
     return basis_wave_number
 
 
+def field_incident_V(resolution_information, image_geometry_materials_full, rho, the_phi):
+    import cmath
+    import numpy as np
+    discretise_M = resolution_information["length_y_side"]
+    discretise_N = resolution_information["length_x_side"]
+    basis_counter = discretise_M*discretise_N
+
+    V = np.zeros((basis_counter, 1), dtype=np.complex_)
+    for ct1 in range(0, basis_counter):
+        # V(ct1) = exp(-1i*kr(1)*rho(ct1)*cos(the_phi(ct1)));
+        V[ct1] = cmath.exp(-1j * image_geometry_materials_full[image_geometry_materials_full['name'] == 'vacuum']['kr'].iloc[0] * rho[ct1] * cmath.cos(the_phi[ct1]))
+    return V
+
+
+def field_incident_D(resolution_information, image_geometry_materials_full, basis_wave_number):
+    # import cmath
+    import numpy as np
+    discretise_M = resolution_information["length_y_side"]
+    discretise_N = resolution_information["length_x_side"]
+    basis_counter = discretise_M*discretise_N
+
+    D = np.zeros((basis_counter, 1), dtype=np.complex_)
+    for ct1 in range(0, basis_counter):
+        # D(ct1,1)= (basis_wave_number(ct1,1)*basis_wave_number(ct1,1) - k0*k0); % contrast function
+        D[ct1] = basis_wave_number[ct1] * basis_wave_number[ct1] - image_geometry_materials_full[image_geometry_materials_full['name'] == 'vacuum']['kr'].iloc[0] * image_geometry_materials_full[image_geometry_materials_full['name'] == 'vacuum']['kr'].iloc[0]
+    return D
+
+
+def rfo(field_incident_D):
+    # Reduced Foreward Operator
+    # Rfo = logical(D);
+    field_incident_D[field_incident_D != 0] = 1
+    rfo = field_incident_D
+    # unique_elements, counts_elements = np.unique(rfo, return_counts=True)
+    # print(unique_elements)
+    # print(counts_elements)
+    return rfo
+
+
+def Vred(rfo, field_incident_V):
+    # Vred = Rfo .* V; % create V reduced
+    Vred = rfo * field_incident_V
+    return Vred
+
+
+def Vred_2D(resolution_information, Vred):
+    import numpy as np
+    discretise_M = resolution_information["length_y_side"]
+    discretise_N = resolution_information["length_x_side"]
+    Vred_2D = np.reshape(Vred, (discretise_M, discretise_N))
+    return Vred_2D
+
+
+# def G_vector(resolution_information, image_geometry_materials_full, basis_wave_number):
+#     # import cmath
+#     import numpy as np
+#     discretise_M = resolution_information["length_y_side"]
+#     discretise_N = resolution_information["length_x_side"]
+#     basis_counter = discretise_M*discretise_N
+
+#     D = np.zeros((basis_counter, 1), dtype=np.complex_)
+#     for ct1 in range(0, basis_counter):
+#         # D(ct1,1)= (basis_wave_number(ct1,1)*basis_wave_number(ct1,1) - k0*k0); % contrast function
+#         D[ct1] = basis_wave_number[ct1] * basis_wave_number[ct1] - image_geometry_materials_full[image_geometry_materials_full['name'] == 'vacuum']['kr'].iloc[0] * image_geometry_materials_full[image_geometry_materials_full['name'] == 'vacuum']['kr'].iloc[0]
+
+#     G_vector = zeros(basis_counter, 1); % BMT dense matrix, stored as a vector
+#     start2 = tic;
+#     for ct1 = 1:basis_counter
+#         if (mod(ct1, 100) == 0)
+#             fprintf(1, '%dth element \n', ct1);
+#         end
+#         R_mn2 = abs(position(ct1)-position(1));
+#         if ct1 == 1
+#             %         G_vector(ct1,1)=(1i/4.0)*((2.0*pi*equiv_a/kr(1))*(besselj(1,kr(1)*equiv_a)-1i*bessely(1,kr(1)*equiv_a))-4.0*1i/(kr(1)*kr(1)));
+#             % G_vector(ct1, 1) = (1i / 4.0) * ((2.0 * pi * equiv_a / kr(1)) * besselh(1, 2, kr(1)*equiv_a) - 4.0 * 1i / (kr(1) * kr(1)));
+#             G_vector(ct1, 1) = (1i / 4.0) * ((2.0 * pi * equiv_a / materials_master(strcmp(materials_master.('name'), 'vacuum'),:).('kr')) * besselh(1, 2, materials_master(strcmp(materials_master.('name'), 'vacuum'),:).('kr')*equiv_a) - 4.0 * 1i / (materials_master(strcmp(materials_master.('name'), 'vacuum'),:).('kr') * materials_master(strcmp(materials_master.('name'), 'vacuum'),:).('kr')));
+#         else
+#             %         G_vector(ct1,1)=(1i/4.0)*(2.0*pi*equiv_a/kr(1))*besselj(1,kr(1)*equiv_a)*(besselj(0,kr(1)*R_mn2)-1i*bessely(0,kr(1)*R_mn2));
+#             % G_vector(ct1, 1) = (1i / 4.0) * (2.0 * pi * equiv_a / kr(1)) * besselj(1, kr(1)*equiv_a) * besselh(0, 2, kr(1)*R_mn2);
+#             G_vector(ct1, 1) = (1i / 4.0) * (2.0 * pi * equiv_a / materials_master(strcmp(materials_master.('name'), 'vacuum'),:).('kr')) * besselj(1, materials_master(strcmp(materials_master.('name'), 'vacuum'),:).('kr')*equiv_a) * besselh(0, 2, materials_master(strcmp(materials_master.('name'), 'vacuum'),:).('kr')*R_mn2);
+#         end
+#     end
+
+#     return D
