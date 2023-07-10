a6851089e1bce6c2200351e86ac2b8dad7ccce3b
AJMcE <anthonyjames.mcelwee@gmail.com>
2023-07-10 15:28:50 +0100

    updated environment yaml

--- lib/unified_vdb.py
+++ lib/unified_vdb.py
@@ -2,6 +2,169 @@ import scipy.io
 import numpy as np
 
 
+def displayDataBesselApproach(WavefieldSctCircle, angle):
+    import matplotlib.pyplot as plt
+    import numpy as np
+    # Plot data at a number of receivers
+    # fig = plt.figure(figsize=(0.39, 0.39), dpi=100)
+    plt.plot(angle.T, np.abs(WavefieldSctCircle).T, label='Bessel-function method')
+    plt.tight_layout()
+    plt.legend(loc='upper right')
+    plt.title('scattered wave data in 2D', fontsize=12)
+    plt.axis('tight')
+    plt.xlabel('observation angle in degrees')
+    plt.xlim([0, 360])
+    plt.ylabel('abs(data) $\\rightarrow$')
+    plt.show()
+
+
+def displayDataCompareApproachs(bessel_approach, CIS_approach, angle):
+    import matplotlib.pyplot as plt
+    error_num = np.linalg.norm(CIS_approach.flatten('F') - bessel_approach.flatten('F'), ord=1) / np.linalg.norm(bessel_approach.flatten('F'), ord=1)
+    # error = format(error_num, 'f')
+    error = str(error_num)
+    print("error_num", error_num)
+    # Plot data at a number of receivers
+    # fig = plt.figure(figsize=(0.39, 0.39), dpi=100)
+    plt.tight_layout()
+    plt.plot(angle.T, np.abs(CIS_approach).T, '--r', angle.T, np.abs(bessel_approach).T, 'b')
+    plt.legend(['Integral-equation method', 'Bessel-function method'], loc='upper center')
+    plt.text(0.5*np.max(angle), 0.8*np.max(np.abs(CIS_approach)), 'Error$^{sct}$ = ' + error, color='red', ha='center', va='center')
+    plt.title('scattered wave data in 2D', fontsize=12)
+    plt.axis('tight')
+    plt.xlabel('observation angle in degrees')
+    plt.xlim([0, 360])
+    plt.ylabel('abs(data) $\\rightarrow$')
+    plt.show()
+
+
+def graddiv(v, dx, N1, N2):
+    # Anywhere where there is swapping there could be inheritance issues so use copy.
+    v = v.copy()
+    u = np.zeros((v.shape), dtype=np.complex_, order='F')
+
+    # % Compute d1d1_v1, d2d2_v2
+    # u{1}(2:N1 - 1, :) = v{1}(1:N1 - 2, :) - 2 * v{1}(2:N1 - 1, :) + v{1}(3:N1, :);
+    u[0, 1:N1 - 1, :] = v[0, 0:N1 - 2, :] - 2 * v[0, 1:N1 - 1, :] + v[0, 2:N1, :]
+
+    # u{2}(:, 2:N2 - 1) = v{2}(:, 1:N2 - 2) - 2 * v{2}(:, 2:N2 - 1) + v{2}(:, 3:N2);
+    u[1, :, 1:N2 - 1] = v[1, :, 0:N2 - 2] - 2 * v[1, :, 1:N2 - 1] + v[1, :, 2:N2]
+
+    # % Replace the input vector v1 by d1_v and v2 by d2_v2 ---------------------
+    # v{1}(2:N1 - 1, :) = (v{1}(3:N1, :) - v{1}(1:N1 - 2, :)) / 2; % d1_v1
+    v[0, 1:N1 - 1, :] = (v[0, 2:N1, :] - v[0, 0:N1 - 2, :]) / 2.0
+
+    # v{2}(:, 2:N2 - 1) = (v{2}(:, 3:N2) - v{2}(:, 1:N2 - 2)) / 2; % d2_v2
+    v[1, :, 1:N2 - 1] = (v[1, :, 2:N2] - v[1, :, 0:N2 - 2]) / 2.0
+
+    # % Add d1_v2 = d1d2_v2 to output vector u1 ---------------------------------
+    # u{1}(2:N1 - 1, :) = u{1}(2:N1 - 1, :) + (v{2}(3:N1, :) - v{2}(1:N1 - 2, :)) / 2;
+    u[0, 1:N1 - 1, :] = u[0, 1:N1 - 1, :] + (v[1, 2:N1, :] - v[1, 0:N1 - 2, :]) / 2.0
+
+    # % Add d2_v1 = d2d1_v1 to output vector u2 ---------------------------------
+    # u{2}(:, 2:N2 - 1) = u{2}(:, 2:N2 - 1) + (v{1}(:, 3:N2) - v{1}(:, 1:N2 - 2)) / 2;
+    u[1, :, 1:N2 - 1] = u[1, :, 1:N2 - 1] + (v[0, :, 2:N2] - v[0, :, 0:N2 - 2]) / 2.0
+
+    # % divide by dx^2
+    u[0, :, :] = u[0, :, :] / dx**2
+    u[1, :, :] = u[1, :, :] / dx**2
+    return u
+
+
+def Green(dx, gamma_0, X1fft, X2fft):
+    from scipy.special import kv, iv
+    gam0 = gamma_0
+    X1 = X1fft
+    X2 = X2fft
+    DIS = np.sqrt(X1**2 + X2**2)
+    # avoid Green's singularity for DIS = 0
+    DIS[0, 0] = 1
+    G = 1/(2*np.pi) * kv(0, gam0*DIS)
+    # radius circle with area of dx^2
+    delta = (np.pi)**(-1/2) * dx
+    factor = 2 * iv(1, gam0*delta) / (gam0*delta)
+    # integral includes gam0^2
+    IntG = (gam0**2 * dx**2) * factor**2 * G
+    IntG[0, 0] = 1 - gam0*delta * kv(1, gam0*delta) * factor
+    return IntG
+
+
+def initFFTGreen(N1, N2, dx, gamma_0):
+    N1fft = np.int64(2**np.ceil(np.log2(2*N1)))
+    N2fft = np.int64(2**np.ceil(np.log2(2*N2)))
+
+    # x1(1:N1fft) = [0 : N1fft/2-1   N1fft/2 : -1 : 1] * input.dx;
+    x1 = np.zeros((1, N1fft), dtype=np.float64, order='F')
+    x1[0, :] = np.concatenate((np.arange(0, N1fft//2), np.arange(N1fft//2, 0, -1)))*dx
+
+    # x2(1:N2fft) = [0 : N2fft/2-1   N2fft/2 : -1 : 1] * input.dx;
+    x2 = np.zeros((1, N2fft), dtype=np.float64, order='F')
+    x2[0, :] = np.concatenate((np.arange(0, N2fft//2), np.arange(N2fft//2, 0, -1)))*dx
+
+    # [temp.X1fft,temp.X2fft] = ndgrid(x1,x2);
+    X1fft, X2fft = np.meshgrid(x1, x2, indexing='ij')
+
+    # compute gam_0^2 * subdomain integrals  of Green function
+    IntG = Green(dx, gamma_0)
+
+    # apply n-dimensional Fast Fourier transform
+    FFTG = np.fft.fftn(IntG)
+    return FFTG
+
+
+def initGrid():
+    # number of samples in x_1
+    N1 = 120
+    # number of samples in x_2
+    N2 = 100
+    # with meshsize dx
+    dx = 2
+
+    x1 = np.zeros((1, N1), dtype=np.float64, order='F')
+    x1[0, :] = -(N1+1)*dx/2 + np.linspace(1, N1, num=N1)*dx
+
+    x2 = np.zeros((1, N2), dtype=np.float64, order='F')
+    x2[0, :] = -(N2+1)*dx/2 + np.linspace(1, N2, num=N2)*dx
+
+    # [X1,X2] = ndgrid(x1,x2)
+    X1, X2 = np.meshgrid(x1, x2, indexing='ij')
+
+    # Now array subscripts are equivalent with Cartesian coordinates
+    # x1 axis points downwards and x2 axis is in horizontal direction
+    return N1, N2, dx, X1, X2
+
+
+def initSourceReceiver():
+    # Source Position
+    xS = np.zeros((1, 2), dtype=np.float64, order='F')
+    xS[0, 0] = -170.0
+    xS[0, 1] = 0.0
+
+    # Receiver Positions
+    NR = 180
+    rcvr_phi = np.zeros((1, NR), dtype=np.float64, order='F')
+    # rcvr_phi[0, :] = np.linspace(1, NR, num=NR)*(2*np.pi)/NR
+    rcvr_phi[0, 0:NR] = np.arange(1, NR+1, 1) * 2.0 * np.pi / NR
+
+    xR = np.zeros((2, NR), dtype=np.float64, order='F')
+    xR[0, 0:NR] = 150 * np.cos(rcvr_phi)
+    xR[1, 0:NR] = 150 * np.sin(rcvr_phi)
+    return xS, NR, rcvr_phi, xR
+
+
+def Kop(v, FFTG):
+    # Make FFT grid
+    N1, N2 = v.shape
+    Cv = np.zeros(FFTG.shape, dtype=np.complex128, order='F')
+    Cv[0:N1, 0:N2] = v.copy()
+    # Convolution by FFT
+    Cv = np.fft.fftn(Cv)
+    Cv = np.fft.ifftn(FFTG * Cv)
+    Kv = np.zeros((N1, N2), dtype=np.complex128, order='F')
+    Kv[0:N1, 0:N2] = Cv[0:N1, 0:N2]
+    return Kv
+
+
 def mat_checker(variable, var_name):
     # mat_checker(x1fft, nameof(x1fft))
     var_name_matlab = scipy.io.loadmat(var_name + '.mat')
@@ -26,4 +189,3 @@ def mat_loader(var_name):
     var_name_matlab = scipy.io.loadmat(var_name + '.mat')
     var_name_m = np.array(var_name_matlab[var_name], dtype=np.complex_)
     return var_name_m
-
