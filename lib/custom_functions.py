e04961fbafda84684de709b64e1f7eb7c860e651
AJMcE <anthonyjames.mcelwee@gmail.com>
2023-07-15 13:41:05 +0100

    Setting up the final dataset generating script

---
+++ lib/custom_functions.py
@@ -0,0 +1,376 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import os
+from scipy.sparse.linalg import bicgstab, LinearOperator
+import time
+from scipy.special import kv, iv
+
+
+def generate_random_circles(N1, N2, radius_min_pix, radius_max_pix, seedling, seed_count, subfolder):
+    for seed in range(seedling, seedling+seed_count):
+
+        shape_array = np.zeros((N2, N1))
+
+        radius = random.uniform(radius_min_pix, radius_max_pix)
+        center_x = random.uniform(radius, N1 - radius)
+        center_y = random.uniform(radius, N2 - radius)
+
+        for i in range(N2):
+            for j in range(N1):
+                if np.sqrt((j - center_x) ** 2 + (i - center_y) ** 2) <= radius:
+                    shape_array[i, j] = 1
+
+        fig, ax = plt.subplots()
+        ax.imshow(shape_array, cmap='binary', interpolation='none')
+        ax.axis('off')
+        # plt.savefig(os.path.join(subfolder, f"instance_{seed}.png"), bbox_inches='tight', pad_inches=0)
+        plt.savefig(os.path.join(subfolder, f"instance_{str(seed).zfill(10)}.png"), bbox_inches='tight', pad_inches=0)
+        plt.close()
+        np.save(os.path.join(subfolder, f"instance_{str(seed).zfill(10)}.npy"), shape_array)
+        # np.save(os.path.join(subfolder, f"instance_{seed}.npy"), shape_array)
+
+
+def initEM(c_0, eps_sct, mu_sct, gamma_0, N1, N2, dx):
+    # add location of source/receiver
+    xS, NR, rcvr_phi, xR = initSourceReceiver()
+
+    # add grid in either 1D, 2D or 3D
+    # initGrid() and initGridEM() are equivalent
+    X1, X2 = initGrid(N1, N2, dx)
+
+    # compute FFT of Green function
+    FFTG = initFFTGreen(N1, N2, dx, gamma_0)
+
+    Errcri = 1e-18
+    return xS, NR, rcvr_phi, xR, X1, X2, FFTG, Errcri
+
+
+def initSourceReceiver():
+    # Source Position
+    xS = np.zeros((1, 2), dtype=np.float64, order='F')
+    xS[0, 0] = -170.0
+    xS[0, 1] = 0.0
+
+    # Receiver Positions
+    NR = 180
+    rcvr_phi = np.zeros((1, NR), dtype=np.float64, order='F')
+    # rcvr_phi[0, :] = np.linspace(1, NR, num=NR)*(2*np.pi)/NR
+    rcvr_phi[0, 0:NR] = np.arange(1, NR+1, 1) * 2.0 * np.pi / NR
+
+    xR = np.zeros((2, NR), dtype=np.float64, order='F')
+    xR[0, 0:NR] = 150 * np.cos(rcvr_phi)
+    xR[1, 0:NR] = 150 * np.sin(rcvr_phi)
+    return xS, NR, rcvr_phi, xR
+
+
+def initGrid(N1, N2, dx):
+    x1 = np.zeros((1, N1), dtype=np.float64, order='F')
+    x1[0, :] = -(N1+1)*dx/2 + np.linspace(1, N1, num=N1)*dx
+
+    x2 = np.zeros((1, N2), dtype=np.float64, order='F')
+    x2[0, :] = -(N2+1)*dx/2 + np.linspace(1, N2, num=N2)*dx
+
+    # [X1,X2] = ndgrid(x1,x2)
+    X1, X2 = np.meshgrid(x1, x2, indexing='ij')
+
+    # Now array subscripts are equivalent with Cartesian coordinates
+    # x1 axis points downwards and x2 axis is in horizontal direction
+    return X1, X2
+
+
+def initFFTGreen(N1, N2, dx, gamma_0):
+    N1fft = np.int64(2**np.ceil(np.log2(2*N1)))
+    N2fft = np.int64(2**np.ceil(np.log2(2*N2)))
+
+    # x1(1:N1fft) = [0 : N1fft/2-1   N1fft/2 : -1 : 1] * input.dx;
+    x1 = np.zeros((1, N1fft), dtype=np.float64, order='F')
+    x1[0, :] = np.concatenate((np.arange(0, N1fft//2), np.arange(N1fft//2, 0, -1)))*dx
+
+    # x2(1:N2fft) = [0 : N2fft/2-1   N2fft/2 : -1 : 1] * input.dx;
+    x2 = np.zeros((1, N2fft), dtype=np.float64, order='F')
+    x2[0, :] = np.concatenate((np.arange(0, N2fft//2), np.arange(N2fft//2, 0, -1)))*dx
+
+    # [temp.X1fft,temp.X2fft] = ndgrid(x1,x2);
+    X1fft, X2fft = np.meshgrid(x1, x2, indexing='ij')
+
+    def Green(dx, gamma_0):
+        from scipy.special import kv, iv
+        gam0 = gamma_0
+        X1 = X1fft
+        X2 = X2fft
+        DIS = np.sqrt(X1**2 + X2**2)
+        # avoid Green's singularity for DIS = 0
+        DIS[0, 0] = 1
+        G = 1/(2*np.pi) * kv(0, gam0*DIS)
+        # radius circle with area of dx^2
+        delta = (np.pi)**(-1/2) * dx
+        factor = 2 * iv(1, gam0*delta) / (gam0*delta)
+        # integral includes gam0^2
+        IntG = (gam0**2 * dx**2) * factor**2 * G
+        IntG[0, 0] = 1 - gam0*delta * kv(1, gam0*delta) * factor
+        return IntG
+
+    # compute gam_0^2 * subdomain integrals  of Green function
+    IntG = Green(dx, gamma_0)
+
+    # apply n-dimensional Fast Fourier transform
+    FFTG = np.fft.fftn(IntG)
+    return FFTG
+
+
+def initEMContrast(eps_sct, mu_sct, X1, X2, geometry_file=None):
+    if geometry_file is None:
+        # half width slab / radius circle cylinder / radius sphere
+        a = 40
+        R = np.sqrt(X1**2 + X2**2)
+        # (1) Compute permittivity contrast
+        CHI_eps = (1-eps_sct) * (R < a)
+        # (2) Compute permeability contrast
+        CHI_mu = (1-mu_sct) * (R < a)
+    else:
+        array = np.load(geometry_file, mmap_mode='r')
+        # (1) Compute permittivity contrast
+        CHI_eps = (1-eps_sct) * array.T
+
+        # (2) Compute permeability contrast
+        CHI_mu = (1-mu_sct) * array.T
+        a = None
+    return a, CHI_eps, CHI_mu
+
+
+def plotEMContrast(CHI_eps, CHI_mu, X1, X2):
+    # Plot 2D contrast/source distribution
+    # x1 = ForwardBiCGSTABFFT.input.X1(:, 1);
+    x1 = X1[:, 0]
+    # x2 = ForwardBiCGSTABFFT.input.X2(1, :);
+    x2 = X2[0, :]
+
+    fig = plt.figure(figsize=(7.09, 4.72))
+    fig.subplots_adjust(wspace=0.3)
+
+    ax1 = fig.add_subplot(1, 2, 1)
+    im1 = ax1.imshow(np.fliplr(CHI_eps), extent=[x2[0], x2[-1], x1[-1], x1[0]], cmap='jet', interpolation='none')
+    ax1.set_xlabel('x_2 \u2192')
+    ax1.set_ylabel('\u2190 x_1')
+    ax1.set_aspect('equal', adjustable='box')
+    fig.colorbar(im1, ax=ax1, orientation='horizontal')
+    ax1.set_title(r'$\chi^\epsilon = 1 - \epsilon_{sct}/\epsilon_{0}$', fontsize=13)
+
+    ax2 = fig.add_subplot(1, 2, 2)
+    im2 = ax2.imshow(np.fliplr(CHI_mu), extent=[x2[0], x2[-1], x1[-1], x1[0]], cmap='jet', interpolation='none')
+    ax2.set_xlabel('x_2 \u2192')
+    ax2.set_ylabel('\u2190 x_1')
+    ax2.set_aspect('equal', adjustable='box')
+    fig.colorbar(im2, ax=ax2, orientation='horizontal')
+    ax2.set_title(r'$\chi^\mu = 1 - \mu_{sct}/\mu_{0}$', fontsize=13)
+    plt.show()
+
+
+def IncEMwave(gamma_0, xS, dx, X1, X2):
+    # incident wave from electric dipole in negative x_1
+
+    # radius circle with area of dx^2
+    delta = (np.pi)**(-1/2) * dx
+    factor = 2 * iv(1, gamma_0*delta) / (gamma_0*delta)
+
+    X1 = X1-xS[0, 0]
+    X2 = X2-xS[0, 1]
+    DIS = np.sqrt(X1**2 + X2**2)
+    X2 = X2/DIS
+    X1 = X1/DIS
+
+    G = factor * 1/(2*np.pi) * kv(0, gamma_0*DIS)
+    dG = - factor * gamma_0 * 1/(2*np.pi) * kv(1, gamma_0*DIS)
+    dG11 = (2 * X1 * X1 - 1) * (-dG/DIS) + gamma_0**2 * X1 * X1 * G
+    dG21 = (2 * X2 * X1 - 0) * (-dG/DIS) + gamma_0**2 * X2 * X1 * G
+
+    E_inc = np.zeros((3, X1.shape[0], X1.shape[1]), dtype=np.complex128, order='F')
+    E_inc[0, :] = -(-gamma_0**2 * G + dG11)
+    E_inc[1, :] = - dG21
+    E_inc[2, :] = 0
+
+    ZH_inc = np.zeros((3, X1.shape[0], X1.shape[1]), dtype=np.complex128, order='F')
+    ZH_inc[0, :] = 0
+    ZH_inc[1, :] = 0
+    ZH_inc[2, :] = gamma_0 * X2 * dG
+    return E_inc, ZH_inc
+
+
+def ITERBiCGSTABwE(E_inc, CHI_eps, Errcri, N1, N2, dx, FFTG, gamma_0, x0=None):
+    # BiCGSTAB_FFT scheme for contrast source integral equation Aw = b
+    # Known 1D vector right-hand side
+    N = CHI_eps.flatten('F').shape[0]
+
+    itmax = N
+
+    b = np.zeros((2*N, 1), dtype=np.complex128, order='F')
+    b[0:N, 0] = CHI_eps.flatten('F') * E_inc[0, :].flatten('F')
+    b[N:2*N, 0] = CHI_eps.flatten('F') * E_inc[1, :].flatten('F')
+
+    if x0 is None:
+        # Create an array of zeros
+        x0 = np.zeros(b.shape, dtype=np.complex128, order='F')
+
+    def custom_matvec(w):
+        return Aw(w, N1, N2, dx, FFTG, CHI_eps, gamma_0)
+
+    Aw_operator = LinearOperator((b.shape[0], b.shape[0]), matvec=custom_matvec)
+
+    def callback(xk):
+        callback.iter += 1
+        resvec = np.linalg.norm(Aw_operator(xk).T - b.T)
+        callback.time_total = time.time() - callback.start_time
+        row = np.array([callback.iter, resvec, callback.time_total])
+        callback.information = np.vstack((callback.information, row))
+
+    # Initialise iteration count
+    callback.start_time = time.time()
+    callback.iter = 0.0
+    # callback.information = np.array([[callback.iter, np.linalg.norm(b), time.time() - callback.start_time]])
+    callback.information = np.array([[callback.iter, np.linalg.norm(Aw_operator(x0).T - b.T), time.time() - callback.start_time]])
+    # callback.information = np.empty((1, 3))
+
+    # Call bicgstab with the LinearOperator instance and other inputs
+    w, exit_code = bicgstab(Aw_operator, b, x0, tol=Errcri, maxiter=itmax, callback=callback)
+
+    # Output Matrix UPDATE
+    # w = w.reshape((N1, N2), order='F')
+    w_E = vector2matrix(w, N1, N2)
+    return w_E, exit_code, callback.information
+
+
+def Aw(w, N1, N2, dx, FFTG, CHI_eps, gamma_0):
+    N = CHI_eps.flatten('F').shape[0]
+    # Convert 1D vector to matrix
+    w_E = vector2matrix(w, N1, N2)
+    Kw_E = KopE(w_E, gamma_0, N1, N2, dx, FFTG)
+    # Convert matrix to 1D vector
+    y = np.zeros((2*N1*N2, 1), dtype=np.complex_, order='F')
+    y[0:N, 0] = w_E[0, :, :].flatten('F') - (CHI_eps.flatten('F') * Kw_E[0, :, :].flatten('F'))
+    y[N:2*N, 0] = w_E[1, :, :].flatten('F') - (CHI_eps.flatten('F') * Kw_E[1, :, :].flatten('F'))
+    return y
+
+
+def vector2matrix(w, N1, N2):
+    # Modify vector output from 'bicgstab' to matrix for further computations
+    # N = CHI_eps.flatten('F').shape[0]
+    N = N1 * N2
+    DIM = [N1, N2]
+    w_E = np.zeros((2, N1, N2), dtype=np.complex128, order='F')
+    w_E[0, :, :] = np.reshape(w[0:N], DIM, order='F')
+    w_E[1, :, :] = np.reshape(w[N:2*N], DIM, order='F')
+    return w_E
+
+
+def KopE(wE, gamma_0, N1, N2, dx, FFTG):
+    wE = wE.copy()
+    KwE = np.zeros((2, N1, N2), dtype=np.complex128, order='F')
+    for n in range(0, 2):
+        KwE[n, :, :] = Kop(wE[n, :, :], FFTG)
+    # dummy is temporary storage
+    dummy = np.zeros((2, N1, N2), dtype=np.complex128, order='F')
+    dummy[:, :, :] = graddiv(KwE, dx, N1, N2)
+    # print((graddiv(KwE, dx, N1, N2)).shape)
+    for n in range(0, 2):
+        KwE[n, :, :] = KwE[n, :, :] - dummy[n, :, :] / gamma_0**2
+    return KwE
+
+
+def Kop(v, FFTG):
+    # Make FFT grid
+    N1, N2 = v.shape
+    Cv = np.zeros(FFTG.shape, dtype=np.complex128, order='F')
+    Cv[0:N1, 0:N2] = v.copy()
+    # Convolution by FFT
+    Cv = np.fft.fftn(Cv)
+    Cv = np.fft.ifftn(FFTG * Cv)
+    Kv = np.zeros((N1, N2), dtype=np.complex128, order='F')
+    Kv[0:N1, 0:N2] = Cv[0:N1, 0:N2]
+    return Kv
+
+
+def graddiv(v, dx, N1, N2):
+    # Anywhere where there is swapping there could be inheritance issues so use copy.
+    v = v.copy()
+    u = np.zeros((v.shape), dtype=np.complex_, order='F')
+
+    # % Compute d1d1_v1, d2d2_v2
+    # u{1}(2:N1 - 1, :) = v{1}(1:N1 - 2, :) - 2 * v{1}(2:N1 - 1, :) + v{1}(3:N1, :);
+    u[0, 1:N1 - 1, :] = v[0, 0:N1 - 2, :] - 2 * v[0, 1:N1 - 1, :] + v[0, 2:N1, :]
+
+    # u{2}(:, 2:N2 - 1) = v{2}(:, 1:N2 - 2) - 2 * v{2}(:, 2:N2 - 1) + v{2}(:, 3:N2);
+    u[1, :, 1:N2 - 1] = v[1, :, 0:N2 - 2] - 2 * v[1, :, 1:N2 - 1] + v[1, :, 2:N2]
+
+    # % Replace the input vector v1 by d1_v and v2 by d2_v2 ---------------------
+    # v{1}(2:N1 - 1, :) = (v{1}(3:N1, :) - v{1}(1:N1 - 2, :)) / 2; % d1_v1
+    v[0, 1:N1 - 1, :] = (v[0, 2:N1, :] - v[0, 0:N1 - 2, :]) / 2.0
+
+    # v{2}(:, 2:N2 - 1) = (v{2}(:, 3:N2) - v{2}(:, 1:N2 - 2)) / 2; % d2_v2
+    v[1, :, 1:N2 - 1] = (v[1, :, 2:N2] - v[1, :, 0:N2 - 2]) / 2.0
+
+    # % Add d1_v2 = d1d2_v2 to output vector u1 ---------------------------------
+    # u{1}(2:N1 - 1, :) = u{1}(2:N1 - 1, :) + (v{2}(3:N1, :) - v{2}(1:N1 - 2, :)) / 2;
+    u[0, 1:N1 - 1, :] = u[0, 1:N1 - 1, :] + (v[1, 2:N1, :] - v[1, 0:N1 - 2, :]) / 2.0
+
+    # % Add d2_v1 = d2d1_v1 to output vector u2 ---------------------------------
+    # u{2}(:, 2:N2 - 1) = u{2}(:, 2:N2 - 1) + (v{1}(:, 3:N2) - v{1}(:, 1:N2 - 2)) / 2;
+    u[1, :, 1:N2 - 1] = u[1, :, 1:N2 - 1] + (v[0, :, 2:N2] - v[0, :, 0:N2 - 2]) / 2.0
+
+    # % divide by dx^2
+    u[0, :, :] = u[0, :, :] / dx**2
+    u[1, :, :] = u[1, :, :] / dx**2
+    return u
+
+
+def plotContrastSourcewE(w_E, X1, X2):
+    import matplotlib.pyplot as plt
+    # Plot 2D contrast/source distribution
+    x1 = X1[:, 0]
+    x2 = X2[0, :]
+    fig = plt.figure(figsize=(7.09, 4.72))
+    fig.subplots_adjust(wspace=0.3)
+    ax1 = fig.add_subplot(1, 2, 1)
+    im1 = ax1.imshow(np.abs(w_E[0]), extent=[x2[0], x2[-1], x1[-1], x1[0]], cmap='jet', interpolation='none')
+    ax1.set_xlabel('x$_2$ \u2192')
+    ax1.set_ylabel('\u2190 x_1')
+    ax1.set_aspect('equal', adjustable='box')
+    fig.colorbar(im1, ax=ax1, orientation='horizontal')
+    ax1.set_title(r'abs(w$_1^E$)', fontsize=13)
+    ax2 = fig.add_subplot(1, 2, 2)
+    im2 = ax2.imshow(np.abs(w_E[1]), extent=[x2[0], x2[-1], x1[-1], x1[0]], cmap='jet', interpolation='none')
+    ax2.set_xlabel('x$_2$ \u2192')
+    ax2.set_ylabel('\u2190 x_1')
+    ax2.set_aspect('equal', adjustable='box')
+    fig.colorbar(im2, ax=ax2, orientation='horizontal')
+    ax2.set_title(r'abs(w$_2^E$)', fontsize=13)
+    plt.show()
+
+
+def E(E_inc, E_sct):
+    E = np.zeros((E_inc.shape), dtype=np.complex_, order='F')
+    for n in range(0, 2):
+        E[n, :, :] = E_inc[n, :, :] + E_sct[n, :, :]
+    return E
+
+
+def plotEtotalwavefield(E, a, X1, X2, N1, N2):
+    phi = np.arange(0, 2*np.pi, 0.01)
+    import matplotlib.pyplot as plt
+    # Plot wave fields in two-dimensional space
+    fig, axs = plt.subplots(1, 2, figsize=(18, 12))
+    im1 = axs[0].imshow(abs(E[0]), extent=[X2.min(), X2.max(), X1.min(), X1.max()], cmap='jet')
+    axs[0].set_xlabel('x$_2$ $\\rightarrow$')
+    axs[0].set_ylabel('$\\leftarrow$ x$_1$')
+    axs[0].set_title('2D Electric field E1', fontsize=13)
+    fig.colorbar(im1, ax=axs[0], orientation='horizontal')
+    if a is not None:
+        axs[0].plot(a*np.cos(phi), a*np.sin(phi), 'w')
+    im2 = axs[1].imshow(abs(E[1]), extent=[X2.min(), X2.max(), X1.min(), X1.max()], cmap='jet')
+    axs[1].set_xlabel('x$_2$ $\\rightarrow$')
+    axs[1].set_ylabel('$\\leftarrow$ x$_1$')
+    axs[1].set_title('2D Electric field E2', fontsize=13)
+    fig.colorbar(im2, ax=axs[1], orientation='horizontal')
+    if a is not None:
+        axs[1].plot(a*np.cos(phi), a*np.sin(phi), 'w')
+    plt.show()
---
+++ solveremf2.py
@@ -0,0 +1,167 @@
+import random
+import numpy as np
+import os
+import sys
+import time
+from IPython import get_ipython
+import matplotlib.pyplot as plt
+from lib import custom_functions
+
+# Clear workspace
+get_ipython().run_line_magic('clear', '-sf')
+# get_ipython().run_line_magic('reset', '-sf')
+
+np.set_printoptions(threshold=sys.maxsize)
+np.set_printoptions(precision=20)
+
+random.seed(42)
+
+# ESTABLISH EM PARAMETERS
+# Time factor = exp(-iwt)
+# Spatial units is in m
+# Source wavelet M Z_0 / gamma_0  = 1   (Z_0 M = gamma_0)
+
+# wave speed in embedding
+c_0 = 3e8
+# c_0 = 2.99792458e8
+# relative permittivity of scatterer
+eps_sct = 1.75
+# relative permeability of scatterer
+mu_sct = 1.0
+
+# temporal frequency
+f = 10e6
+# wavelength
+wavelength = c_0 / f
+# Laplace parameter
+s = 1e-16 - 1j*2*np.pi*f
+# propagation coefficient
+gamma_0 = s/c_0
+
+# number of samples in x_1
+N1 = 120
+# number of samples in x_2
+N2 = 100
+# with meshsize dx
+dx = 2
+
+
+# GENERATE GEOMETRY
+seedling = 0
+seed_count = 2
+radius_min_pix = int(0.05 * np.minimum(N1, N2))
+radius_max_pix = int(0.15 * np.minimum(N1, N2))
+
+subfolder = "instances"
+os.makedirs(subfolder, exist_ok=True)
+
+custom_functions.generate_random_circles(N1, N2, radius_min_pix, radius_max_pix, seedling, seed_count, subfolder)
+
+# INITIALISE SCENE AND SAVE OUTPUTS
+xS, NR, rcvr_phi, xR, X1, X2, FFTG, Errcri = custom_functions.initEM(c_0, eps_sct, mu_sct, gamma_0, N1, N2, dx)
+E_inc, ZH_inc = custom_functions.IncEMwave(gamma_0, xS, dx, X1, X2)
+
+input_folder = "instances"
+output_folder = "instances_output"
+
+# Create the output folder if it doesn't exist
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+np.save(os.path.join(output_folder, 'E_inc'), E_inc)
+np.save(os.path.join(output_folder, 'ZH_inc'), ZH_inc)
+np.save(os.path.join(output_folder, 'X1'), X1)
+np.save(os.path.join(output_folder, 'X2'), X2)
+
+# SOVLE THE SCENES AND SAVE OUTPUTS
+# Get the list of numpy files in the input folder
+numpy_files = [f for f in os.listdir(input_folder) if f.endswith(".npy")]
+
+# Iterate over each numpy file
+for file_name in numpy_files:
+    # Load the numpy array
+    geometry_file = os.path.join(input_folder, file_name)
+    # array = np.load(file_path)
+
+    # Perform the scattering simulation
+    # add contrast distribution
+    a, CHI_eps, CHI_mu = custom_functions.initEMContrast(eps_sct, mu_sct, X1, X2, geometry_file)
+
+    tic0 = time.time()
+    w_E_o, exit_code_o, information_o = custom_functions.ITERBiCGSTABwE(E_inc, CHI_eps, Errcri, N1, N2, dx, FFTG, gamma_0)
+    toc0 = time.time() - tic0
+    # print("information[-1, :]", information_o[-1, :])
+    print("toc", toc0)
+
+    # Save the result in the output folder with the same file name
+    output_file_path = os.path.join(output_folder, file_name)
+    final_array = np.concatenate((np.expand_dims(CHI_eps, axis=0), np.expand_dims(CHI_mu, axis=0), w_E_o), axis=0)
+    np.save(output_file_path, final_array)
+
+    output_file_path_info = os.path.join(output_folder, os.path.splitext(file_name)[0] + "_info")
+    np.save(output_file_path_info, information_o)
+
+
+numpy_files = [f for f in os.listdir(output_folder) if f.endswith(".npy") and not f.endswith("_info.npy") and f.startswith("instance_")]
+# Iterate over each numpy file
+for file_name in numpy_files:
+    # Load the numpy array
+    geometry_file = os.path.join(output_folder, file_name)
+    array = np.load(geometry_file)
+    print("array.shape", array.shape)
+    # print(array.shape)
+    custom_functions.plotEMContrast(np.real(array[0, :, :]), np.real(array[1, :, :]), X1, X2)
+    # custom_functions.plotContrastSourcewE(array[2:5], X1, X2)
+
+
+# custom_functions.plotEMContrast(CHI_eps, CHI_mu, X1, X2)
+
+# x0 = np.concatenate([w_E_o[0, :, :].flatten('F'), w_E_o[1, :, :].flatten('F')], axis=0)
+
+# tic1 = time.time()
+# w_E, exit_code, information = custom_functions.ITERBiCGSTABwE(E_inc, CHI_eps, Errcri, N1, N2, dx, FFTG, gamma_0, x0)
+# toc1 = time.time() - tic1
+# # print("information[-1, :]", information[-1, :])
+# print("toc", toc1)
+
+# print("fewer iterations by", information_o[-1, 0] - information[-1, 0])
+# print("closer by", information_o[-1, 1] - information[-1, 1])
+# print("faster by", information_o[-1, 2] - information[-1, 2])
+
+# E_sct = custom_functions.KopE(w_E, gamma_0, N1, N2, dx, FFTG)
+
+# custom_functions.plotContrastSourcewE(w_E, X1, X2)
+# E_val = custom_functions.E(E_inc, E_sct)
+# custom_functions.plotEtotalwavefield(E_val, a, X1, X2, N1, N2)
+
+
+# ITERATION INFORMATION
+E_inc = np.load(os.path.join(output_folder, 'E_inc.npy'))
+ZH_inc = np.load(os.path.join(output_folder, 'ZH_inc.npy'))
+X1 = np.load(os.path.join(output_folder, 'X1.npy'))
+X2 = np.load(os.path.join(output_folder, 'X2.npy'))
+
+numpy_files = [f for f in os.listdir(output_folder) if f.endswith("_info.npy")]
+# Iterate over each numpy file
+for file_name in numpy_files:
+    # Load the numpy array
+    geometry_file = os.path.join(output_folder, file_name)
+    array = np.load(geometry_file)
+    print(array.shape)
+
+    # Plot the data
+    plt.plot(array[:, 0], array[:, 1])
+    plt.yscale('log')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('Plot of first column (X) vs. second column (Y)')
+    plt.show()
+
+    # Plot the data
+    plt.plot(array[:, 0], array[:, 2])
+    plt.yscale('log')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('Plot of first column (X) vs. third column (Y)')
+    plt.show()
+
